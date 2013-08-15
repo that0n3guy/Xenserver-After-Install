886d5bb46fdc7a19de3dc7411b4f24b5496341da
that0n3guy <peterolson@simplistechsolutions.com>
2013-08-15 09:05:33 -0500

    change script to set noop for only first drive.

--- tweaks-after-ubuntu-domu-install.sh
+++ tweaks-after-ubuntu-domu-install.sh
@@ -4,8 +4,18 @@
 #
 #http://askubuntu.com/questions/78682/how-do-i-change-to-the-noop-scheduler
 #http://blog.encomiabile.it/2010/04/16/xen-improve-disk-performance-on-domu/
+#The below will noop scheduler for only the main linux drive (xvda)
+
 echo "Setting noop scheduler"
-if ! sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT=.*\+/GRUB_CMDLINE_LINUX_DEFAULT="splash quiet elevator=noop"/' /etc/default/grub; then
-  echo "FATAL: could not setting noop scheduler"
+if ! echo noop > /sys/block/xvda/queue/scheduler; then
+  echo "FATAL: could not setting noop scheduler for main drive"
   exit 1
 fi
+
+
+# The below will set  the scheduler for all drives... I don't want this, only the virtual drive.
+#echo "Setting noop scheduler"
+#if ! sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT=.*\+/GRUB_CMDLINE_LINUX_DEFAULT="splash quiet elevator=noop"/' /etc/default/grub; then
+#  echo "FATAL: could not setting noop scheduler"
+#  exit 1
+#fi
